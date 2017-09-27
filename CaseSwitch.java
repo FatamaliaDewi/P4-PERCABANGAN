98f4b1baaa27581a729b1ecd863a1b3d0b8dc489
FatamaliaDewi <32090249+FatamaliaDewi@users.noreply.github.com>
2017-09-27 08:05:10 +0700

    Add files via upload

---
+++ CaseSwitch.java
@@ -0,0 +1,19 @@
+public class CaseSwitch{
+
+    public static void main(String [] args) {
+        String color = "Blue";
+        switch(color){
+        case  "Blue":
+            System.out.println("My favorite color is Blue");
+        case  "Red":
+            System.out.println("My favorite color is Red");
+        case  "Green":
+            System.out.println("My favorite color is Green");
+        default:
+            System.out.println("My favorite color is Black");
+        }
+    }
+}
+
+// pada string 'S'nya huruf besar
+// case 80 diganti case "Blue" dan seterusnya
